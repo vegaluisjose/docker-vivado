b7b3376389adaaa1a7db9758c31665092f5ee36b
Luis Vega <vegaluisjose@gmail.com>
2020-08-21 14:02:59 -0700

    remove this command

--- install.sh
+++ install.sh
@@ -5,5 +5,3 @@ VIVADO_BIN=Xilinx_Unified_2020.1_0602_1208_Lin64.bin
 chmod +x $VIVADO_BIN
 
 docker build -t "vivado" -f Dockerfile.vivado .
-
-docker run --rm --pid=host -v "$(pwd)":/workspace "vivado" bash --login /workspace/example.sh
